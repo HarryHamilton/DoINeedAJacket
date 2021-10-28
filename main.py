e303b14208a73187e489bcfcc02d6fa34fa07ce0
Harry <b9026715@ncl.ac.uk>
2021-10-28 20:58:00 +0100

    Program retrieves and stores the temperature of specified sensors

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import requests
-import json
 
-response = requests.get("https://api.usb.urbanobservatory.ac.uk/api/v2.0a/sensors/feed/816ce6c0-ac7b-4525-90d1-1055f6b83bc0")
-print(response.json())
+""" Somes end points for future reference
+print(data["timeseries"][0])  # This accesses the timeseries part of the json
+print(data["timeseries"][0]["latest"])  # accesses "latest" part of json
+"""
+
+endpoint = ["https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/feed/room-3-018-zone-1/room-temperature",
+            "https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/feed/room-3-018-zone-2/room-temperature",
+            "https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/feed/room-3-018-zone-3/room-temperature",
+            "https://api.usb.urbanobservatory.ac.uk/api/v2/sensors/feed/room-3-018-zone-4/room-temperature", ]
+
+temps = []
+for i in range(len(endpoint)):
+    response = requests.get(endpoint[i])
+    data = response.json()
+    temps.append(data["timeseries"][0]["latest"]["value"])  # accesses the value :)
+
+
+
+
